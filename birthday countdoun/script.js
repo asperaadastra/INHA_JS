510d4b0504a188923866c7ba02019c75b3d7bf16
asperaadastra <siddikovmirzoakbar@gmail.com>
2022-02-01 00:14:55 +0500

    .

---
+++ birthday countdoun/script.js
@@ -0,0 +1,27 @@
+const newYears = prompt("18 May 2022");
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = Math.floor((newYearsDate - currentDate)/1000);
+
+    const days = Math.floor(totalSeconds/86400);
+    const hours = Math.floor((totalSeconds-(days*86400))/3600);
+    const mins = Math.floor ((totalSeconds- (days*86400 + hours * 3600))/60);
+    const secs = totalSeconds- (days*86400 + hours * 3600 + mins*60);
+    
+    document.getElementById("days").innerHTML = days
+    document.getElementById("hours").innerHTML = hours
+    document.getElementById("mins").innerHTML = mins
+    document.getElementById("seconds").innerHTML = secs
+
+
+}
+
+
+
+
+
+countdown();
+
+setInterval(countdown, 1000);
